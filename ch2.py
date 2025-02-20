0eb1ecc8fda88c741cf79a1dee8a2135d43409e9
Bruno Jouzier <bruno.jozuier@esbj.fr>
2025-02-20 17:37:31 +0100

    ESBJ-2022 2025-02-20 17:37:40

---
+++ ch2.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+import pandas as pd
+
+# Specify the delimiter explicitly
+data = pd.read_csv('./data/Comptes_Perso_2023_Cloture.csv'
+                  , usecols=["Date","Doc","Description","Montant","Cpte","Ss-Cpte","Dest"]
+                  , dtype={"Date": "string", "Doc": "int64", "Description": "string"
+                          ,"Montant": "float64", "Cpte": "string", "Ss-Cpte": "string", "Dest": "string"}
+                  ,sep=';'
+                  ,index_col=1)
+
+print(data.columns)
+print(data.head(5))
+print(data.shape)
+print(data.index)
+print(data.dtypes)
+
+
