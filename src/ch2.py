ea52247777e53c30b6f6d283111c32b71f53f320
Bruno Jouzier <bruno.jouzier@esbj.fr>
2025-02-20 18:16:07 +0100

    ESBJ-2022 2025-02-20 18:16:09

--- src/ch2.py
+++ src/ch2.py
@@ -1,14 +1,24 @@
-from decimal import Decimal
 import pandas as pd
 
 # Specify the delimiter explicitly
-data = pd.read_csv('./data/Comptes_Perso_2023_Cloture.csv'
+"""
+data = pd.read_csv('./data/Comptes_Perso_2023_Cloture.xlsm'
                   , usecols=["Date","Doc","Description","Montant","Cpte","Ss-Cpte","Dest"]
-                  , dtype={"Date": "datetim64", "Doc": "int64", "Description": "string"
-                          ,"Montant": "float64", "Cpte": "string", "Ss-Cpte": "string", "Dest": "string"}
+                  , dtype={"Doc": "int64", "Description": "string"
+                          ,"Montant": str, "Cpte": "string", "Ss-Cpte": "string", "Dest": "string"}
                   ,sep=';'
-                  ,index_col=1)
-
+                  ,index_col=1
+                  ,parse_dates=True)
+"""
+data = pd.read_excel('./data/Comptes_Perso_2023_Cloture.xlsm'
+                  ,sheet_name="Journal"
+                  , usecols=["Date","Doc","Description","Montant","Cpte","Ss-Cpte","Dest"]
+                  , dtype={"Doc": "int64", "Description": "string"
+                          ,"Montant": float #en lisant depuis Excel le nombre est bien 
+                          #numérique, en csv c'est un chaine a ause des blancs et du €
+                          , "Cpte": "string", "Ss-Cpte": "string", "Dest": "string"}
+                  ,index_col=1
+                  ,parse_dates=True)
 print(data.columns)
 print(data.head(6))
 print(data.shape)
