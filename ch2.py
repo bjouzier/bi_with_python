52161178f2714348c55cc417a3a99ba7e955a604
Bruno Jouzier <bruno.jozuier@esbj.fr>
2025-02-20 17:37:59 +0100

    ESBJ-2022 2025-02-20 17:38:01

--- ch2.py
+++ ch2.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Specify the delimiter explicitly
 data = pd.read_csv('./data/Comptes_Perso_2023_Cloture.csv'
                   , usecols=["Date","Doc","Description","Montant","Cpte","Ss-Cpte","Dest"]
-                  , dtype={"Date": "string", "Doc": "int64", "Description": "string"
+                  , dtype={"Date": "datetim64", "Doc": "int64", "Description": "string"
                           ,"Montant": "float64", "Cpte": "string", "Ss-Cpte": "string", "Dest": "string"}
                   ,sep=';'
                   ,index_col=1)
