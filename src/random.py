bf7cd82d5ee43aae19fb8206f562aab31fea643d
Bruno Jouzier <bruno.jouzier@esbj.fr>
2025-02-22 12:49:02 +0100

    ESBJ-2022 2025-02-22 12:49:04

--- src/random.py
+++ src/random.py
@@ -1,15 +1,7 @@
 import random
-import tkinter as tk
-from tkinter import messagebox
 
 def generate_random_number():
     return random.randint(1, 12)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-
-    random_number = generate_random_number()
-    messagebox.showinfo("Random Number", f"The generated random number is: {random_number}")
-
-    root.destroy()  # Destroy the main window after the message box is closed
+    print(generate_random_number())
